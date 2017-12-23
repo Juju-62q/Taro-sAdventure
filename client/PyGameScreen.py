b67424068d9bc775db2046df12cafd88eb17cc57
JUJU-Arch <juju.jetstream@gmail.com>
2017-12-23 23:08:39 +0900

    using property

--- client/PyGameScreen.py
+++ client/PyGameScreen.py
@@ -5,10 +5,42 @@ class PyGameScreen:
 
     def __init__(self, width, height, surface, fpsClock):
         # pygame and window initialize
-        self.width = width
-        self.height = height
-        self.surface = surface
-        self.fpsClock = fpsClock
+        self._width = width
+        self._height = height
+        self._surface = surface
+        self._fpsClock = fpsClock
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
+
+    @property
+    def surface(self):
+        return self._surface
+
+    @surface.setter
+    def surface(self, surface):
+        self._surface = surface
+
+    @property
+    def fpsClock(self):
+        return self._fpsClock
+
+    @fpsClock.setter
+    def fpsClock(self, fpsClock):
+        self._fpsClock = fpsClock
 
     def checkQuit(self):
         for event in pygame.event.get():
